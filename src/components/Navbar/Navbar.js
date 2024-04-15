ad7bf0ecfa19ea6938f5bc1e7e51ffb0721fc87b
lucioarchimede <larchimede@udesa.edu.ar>
2024-04-15 15:40:47 -0300

    navBar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import "./style.css"
+// import React from 'react';
+// import "./style.css"
 
-function Navbar(props){
+// function Navbar(props){
+
+//     return (
+//         <nav>
+//             <ul className="main-nav">
+//                 { 
+//                     props.elementosMenu.map((unMenu, idx)=> <li key={idx} >{unMenu}</li>)
+                    
+//                 }
+//             </ul>
+//         </nav>
+//     )
+// }
+
+// export default Navbar
+ import React from 'react';
+ import './style.css'
+ import { Link } from "react-router-dom";
+
+function Navbar(){
 
     return (
-        <nav>
-            <ul className="main-nav">
-                { 
-                    props.elementosMenu.map((unMenu, idx)=> <li key={idx} >{unMenu}</li>)
-                    
-                }
-            </ul>
-        </nav>
+        <header className="containerHeader">
+            <article >
+                <nav >
+                    <ul className="containerHeader">
+                    <Link to={`/`}> HOME  </Link>
+                    <p>
+                    /   
+                    </p>
+                    <Link to={`/favoritos`}>  FAVORITOS  </Link>  
+                    </ul>
+
+                    <ul className="containerHeader">
+                    <Link to={`/top_rated`}> TOP RATED </Link>
+                    <p>
+                        /
+                    </p>
+                    <Link to={`/cartelera`}> EN CARTELERA </Link>  
+                    </ul>
+
+
+
+                </nav>
+            </article>
+        </header>
     )
 }
 
